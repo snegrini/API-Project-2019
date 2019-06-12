6340d944c47698f60c0e33bc0a25fc1d48e2faa5
snegrini <snegrini@outlook.com>
2019-06-13 00:15:35 +0200

    Started fixing input without POSIX scanf("%ms"

--- main.c
+++ main.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void addent(char *id_ent)
+#define DEFAULT_LINE_LENGTH 50
+
+#define RED 0
+#define BLACK 1
+
+struct RB_node {
+    char *key;
+    int color;
+    struct RB_node *parent;
+    struct RB_node *left;
+    struct RB_node *right;
+};
+
+struct RB_node *rb_root;
+
+void rb_insert(char *id_ent)
 {
 
 }
@@ -27,30 +42,50 @@ void report(void)
 
 }
 
+int readLine(char *str) {
+    int ch, i = 0;
+    
+    while((ch = getchar()) != '\n') {
+        str[i++] = ch;
+        
+        if (i % DEFAULT_LINE_LENGTH == 0) {
+            int b = DEFAULT_LINE_LENGTH * (i / DEFAULT_LINE_LENGTH);
+            str = realloc(str, sizeof(char) * b);
+        }
+    }
+    str[i] = '\0';
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    char *command, *id_ent, *id_orig, *id_dest, *id_rel;
+    char *command = malloc(sizeof(char) * DEFAULT_LINE_LENGTH);
+    //char *command, *id_ent, *id_orig, *id_dest, *id_rel;
 
     do {
-        scanf("%ms", &command);
-
+        int a = readLine(command);
+        printf("%c", a);
+        
         if (strncmp(command, "addent", 7) == 0) {
-            scanf("%ms", &id_ent);
+            //scanf("%ms", &id_ent);
         } else if (strncmp(command, "delent", 7) == 0) {
-            scanf("%ms", &id_ent);
+            //scanf("%ms", &id_ent);
         } else if (strncmp(command, "addrel", 7) == 0) {
-            scanf("%ms", &id_orig);
-            scanf("%ms", &id_dest);
-            scanf("%ms", &id_rel);
+            //scanf("%ms", &id_orig);
+            //scanf("%ms", &id_dest);
+            //scanf("%ms", &id_rel);
         } else if (strncmp(command, "delrel", 7) == 0) {
-            scanf("%ms", &id_orig);
-            scanf("%ms", &id_dest);
-            scanf("%ms", &id_rel);
+            //scanf("%ms", &id_orig);
+            //scanf("%ms", &id_dest);
+            //scanf("%ms", &id_rel);
         } else if (strncmp(command, "report", 7) == 0) {
             
         }
-
+        printf("%s\n", command);
     } while (strncmp(command, "end", 4) != 0);
+    
+    free(command);
 
     return 0;
 }
